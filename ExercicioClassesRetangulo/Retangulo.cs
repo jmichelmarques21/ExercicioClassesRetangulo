23b02e77df180c3f8ab20699abb525b6ee9f0bec
Jean Marques <jmichelmarques21@outlook.com>
2023-05-06 00:04:10 -0300

    .

---
+++ ExercicioClassesRetangulo/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClasseRetangulo {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            double area = Largura * Altura;
+            return area;
+        }
+
+        public double Perimetro() {
+            double perimetro = (Largura + Altura) * 2;
+            return perimetro;
+        }
+
+        public double Diagonal() {
+            double diagonal = Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+            return diagonal;
+        }
+    }
+}
